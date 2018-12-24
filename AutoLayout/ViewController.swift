e10d15798478642220c8eb81678099ea6d273015
Anthony Lee <anthonylee@umass.edu>
2018-12-24 18:53:34 -0500

    Finished basic setup for autolayout constraints

--- AutoLayout/ViewController.swift
+++ AutoLayout/ViewController.swift
@@ -10,22 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let image = UIImage(named: "hat2")
+    let imageView : UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "hat2"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let descriptionTextView : UITextView = {
+        let textview = UITextView()
+        textview.text = "How are you doing today?"
+        textview.textAlignment = .center
+        textview.font = UIFont.boldSystemFont(ofSize: 20)
+        textview.translatesAutoresizingMaskIntoConstraints = false
+        textview.isEditable = false
+        textview.isScrollEnabled = false
+        return textview
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let imageView = UIImageView(image: image)
         view.addSubview(imageView)
-        
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        
+        view.addSubview(descriptionTextView)
+        setupLayout()
+    }
+
+    func setupLayout() {
         imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        //imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
         imageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
         imageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        
+        //TextView Contraints
+        descriptionTextView.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 150).isActive = true
+        descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant : 30).isActive = true
     }
-
     
 }
 
