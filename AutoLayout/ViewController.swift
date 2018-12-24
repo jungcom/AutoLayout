9122b98a49bee75d30ecade63c6ca476b927a982
Anthony Lee <anthonylee@umass.edu>
2018-12-24 18:27:43 -0500

    AutoLayout first try

--- AutoLayout/ViewController.swift
+++ AutoLayout/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let image = UIImage(named: "hat2")
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let imageView = UIImageView(image: image)
+        view.addSubview(imageView)
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        //imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
     }
 
-
+    
 }
 
