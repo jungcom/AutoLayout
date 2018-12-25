2750ad82eaeb9eeb2eef2bf2ecb3279a8db1d861
Anthony Lee <anthonylee@umass.edu>
2018-12-25 13:34:51 -0500

    Part 2 of AutoLayout : Added multipliers for portrait and landscape modes. Also used flexible attributed strings for text.

--- AutoLayout/ViewController.swift
+++ AutoLayout/ViewController.swift
@@ -13,38 +13,59 @@ class ViewController: UIViewController {
     let imageView : UIImageView = {
         let imageView = UIImageView(image: UIImage(named: "hat2"))
         imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
         return imageView
     }()
     
     let descriptionTextView : UITextView = {
         let textview = UITextView()
-        textview.text = "How are you doing today?"
+        let attributedText = NSMutableAttributedString(string: "How are you doing today?", attributes: [NSAttributedString.Key.font : UIFont.boldSystemFont(ofSize: 20)])
+        attributedText.append(NSAttributedString(string: "\n\n\nToday is Christmas! Are you ready for some presents???????????????", attributes : [NSAttributedString.Key.font:UIFont.boldSystemFont(ofSize: 16), NSAttributedString.Key.foregroundColor : UIColor.gray]))
+        textview.attributedText = attributedText
         textview.textAlignment = .center
-        textview.font = UIFont.boldSystemFont(ofSize: 20)
         textview.translatesAutoresizingMaskIntoConstraints = false
         textview.isEditable = false
         textview.isScrollEnabled = false
         return textview
     }()
     
+    let topContainerView : UIView = {
+        let topContainerView = UIView()
+        topContainerView.translatesAutoresizingMaskIntoConstraints = false
+        return topContainerView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        view.addSubview(imageView)
         view.addSubview(descriptionTextView)
+        view.addSubview(topContainerView)
         setupLayout()
     }
 
     func setupLayout() {
-        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
-        imageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        imageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        //TopcontainerView Constraint
+        topContainerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5).isActive = true
+        topContainerView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        topContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        topContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        
+        topContainerView.addSubview(imageView)
+//      Better to use leading and trailing anchors that right left
+        
+//      topContainerView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+//      topContainerView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        
+        
+        //ImageView Constraint
+        imageView.centerXAnchor.constraint(equalTo: topContainerView.centerXAnchor).isActive = true
+        imageView.centerYAnchor.constraint(equalTo: topContainerView.centerYAnchor).isActive = true
+        imageView.heightAnchor.constraint(equalTo: topContainerView.heightAnchor, multiplier: 0.3).isActive = true
         
         //TextView Contraints
-        descriptionTextView.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 150).isActive = true
-        descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        descriptionTextView.topAnchor.constraint(equalTo: topContainerView.bottomAnchor).isActive = true
+        descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+        descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
         descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant : 30).isActive = true
     }
     
